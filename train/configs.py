1ca266ad3ef579bd9fd192d0817bd35907ec0668
KalickiD <danielkalicki@gmail.com>
2020-05-27 19:46:30 +0200

    saving added, posibility for different loss function for training and test added

--- train/configs.py
+++ train/configs.py
@@ -8,6 +8,8 @@ default_config = {
     'name': '',
     'sentence_linker': {
         'input_drop': 0.0,
+        'prev_link_hdim': 512,
+        'num_gen_links': 2,
         'layers': [
             # {
             #     'SteGluFfn': {
@@ -39,6 +41,7 @@ for i in range(1000):
 # -----------------------------------------------------------------------------
 for i in range(0, 1):
     configs[i]['training']['epochs'] = 1000
+    configs[i]['training']['lr'] = 5e-5
     configs[i]['sentence_linker']['input_drop'] = 0.2
     configs[i]['sentence_linker']['layers'] = [
         {'DenseLayer': {
@@ -50,7 +53,164 @@ for i in range(0, 1):
             'highway_network': None, 'cat_inout': False, 'hidden_act': 'relu'}
         }
     ]
-    configs[i]['name'] = 'bL128_inDr.2|2xDenseLayer_Lr1e-4_1000e_'+str(i)
+    configs[i]['name'] = 'bL128_4p1sentValid_inDr.2|2xDenseLayeDr.5r_Lr5-51000e_'+str(i)
+# -----------------------------------------------------------------------------
+for i in range(1, 2):
+    configs[i]['training']['epochs'] = 1000
+    configs[i]['sentence_linker']['input_drop'] = 0.2
+    configs[i]['sentence_linker']['layers'] = [
+        {'DenseLayer': {
+            'input_dim': 4096, 'hidden_dim': 4096, 'output_dim': 4096, 'hid_drop': 0.5, 'drop_pos': 'output', 'norm': None,
+            'highway_network': None, 'cat_inout': False, 'hidden_act': 'relu'}
+        },
+        {'DenseLayer': {
+            'input_dim': 4096, 'hidden_dim': 4096, 'output_dim': 4096, 'hid_drop': 0.5, 'drop_pos': 'output', 'norm': None,
+            'highway_network': None, 'cat_inout': False, 'hidden_act': 'relu'}
+        },
+        {'DenseLayer': {
+            'input_dim': 4096, 'hidden_dim': 4096, 'output_dim': 4096, 'hid_drop': 0.5, 'drop_pos': 'output', 'norm': None,
+            'highway_network': None, 'cat_inout': False, 'hidden_act': 'relu'}
+        }
+    ]
+    configs[i]['name'] = 'bL128_4p1sentValid_inDr.2|3xDenseLayerDr.5_Lr1e-4_1000e_'+str(i)
+# -----------------------------------------------------------------------------
+for i in range(2, 3):
+    configs[i]['training']['epochs'] = 1000
+    configs[i]['training']['lr'] = 2e-4
+    configs[i]['sentence_linker']['input_drop'] = 0.2
+    configs[i]['sentence_linker']['layers'] = [
+        {'DenseLayer': {
+            'input_dim': 4096, 'hidden_dim': 4096, 'output_dim': 4096, 'hid_drop': 0.3, 'drop_pos': 'output', 'norm': None,
+            'highway_network': None, 'cat_inout': False, 'hidden_act': 'relu'}
+        },
+        {'DenseLayer': {
+            'input_dim': 4096, 'hidden_dim': 4096, 'output_dim': 4096, 'hid_drop': 0.3, 'drop_pos': 'output', 'norm': None,
+            'highway_network': None, 'cat_inout': False, 'hidden_act': 'relu'}
+        }
+    ]
+    configs[i]['name'] = 'bL128_4p1sentValid_inDr.2|2xDenseLayerDr.3_Lr2e-4_1000e_'+str(i)
+# -----------------------------------------------------------------------------
+for i in range(3, 4):
+    configs[i]['training']['epochs'] = 1000
+    configs[i]['training']['lr'] = 5e-5
+    configs[i]['sentence_linker']['input_drop'] = 0.2
+    configs[i]['sentence_linker']['num_gen_links'] = 1
+    configs[i]['sentence_linker']['layers'] = [
+        {'DenseLayer': {
+            'input_dim': 4096, 'hidden_dim': 4096, 'output_dim': 4096, 'hid_drop': 0.5, 'drop_pos': 'output', 'norm': None,
+            'highway_network': None, 'cat_inout': False, 'hidden_act': 'relu'}
+        },
+        {'DenseLayer': {
+            'input_dim': 4096, 'hidden_dim': 4096, 'output_dim': 4096, 'hid_drop': 0.5, 'drop_pos': 'output', 'norm': None,
+            'highway_network': None, 'cat_inout': False, 'hidden_act': 'relu'}
+        }
+    ]
+    configs[i]['name'] = 'bL128_4pRndValid_1LinksPredValidInData_inDr.2|2xDenseLayeDr.5r_Lr5-5_1000e_'+str(i)
+# -----------------------------------------------------------------------------
+for i in range(4, 5):
+    configs[i]['training']['epochs'] = 500
+    configs[i]['training']['lr'] = 5e-5
+    configs[i]['sentence_linker']['input_drop'] = 0.2
+    configs[i]['sentence_linker']['num_gen_links'] = 1
+    configs[i]['sentence_linker']['layers'] = [
+        {'DenseLayer': {
+            'input_dim': 4096, 'hidden_dim': 4096, 'output_dim': 4096, 'hid_drop': 0.5, 'drop_pos': 'output', 'norm': None,
+            'highway_network': None, 'cat_inout': False, 'hidden_act': 'relu'}
+        },
+        {'DenseLayer': {
+            'input_dim': 4096, 'hidden_dim': 4096, 'output_dim': 4096, 'hid_drop': 0.5, 'drop_pos': 'output', 'norm': None,
+            'highway_network': None, 'cat_inout': False, 'hidden_act': 'relu'}
+        },
+        {'DenseLayer': {
+            'input_dim': 4096, 'hidden_dim': 4096, 'output_dim': 4096, 'hid_drop': 0.5, 'drop_pos': 'output', 'norm': None,
+            'highway_network': None, 'cat_inout': False, 'hidden_act': 'relu'}
+        },
+        {'DenseLayer': {
+            'input_dim': 4096, 'hidden_dim': 4096, 'output_dim': 4096, 'hid_drop': 0.5, 'drop_pos': 'output', 'norm': None,
+            'highway_network': None, 'cat_inout': False, 'hidden_act': 'relu'}
+        }
+    ]
+    configs[i]['name'] = 'bL128_4pRndValid_1LinksPredValidInData_inDr.2|4xDenseLayeDr.5r_Lr5-5_500e_'+str(i)
+# -----------------------------------------------------------------------------
+for i in range(5, 6):
+    configs[i]['training']['epochs'] = 200
+    configs[i]['training']['lr'] = 5e-5
+    configs[i]['sentence_linker']['input_drop'] = 0.2
+    configs[i]['sentence_linker']['num_gen_links'] = 1
+    configs[i]['sentence_linker']['layers'] = [
+        {'DenseLayer': {
+            'input_dim': 4096, 'hidden_dim': 4096, 'output_dim': 2048, 'hid_drop': 0.5, 'drop_pos': 'output', 'norm': None,
+            'highway_network': None, 'cat_inout': False, 'hidden_act': 'relu'}
+        },
+        {'DenseLayer': {
+            'input_dim': 2048, 'hidden_dim': 2048, 'output_dim': 2048, 'hid_drop': 0.5, 'drop_pos': 'output', 'norm': None,
+            'highway_network': None, 'cat_inout': False, 'hidden_act': 'relu'}
+        },
+        {'DenseLayer': {
+            'input_dim': 2048, 'hidden_dim': 2048, 'output_dim': 2048, 'hid_drop': 0.5, 'drop_pos': 'output', 'norm': None,
+            'highway_network': None, 'cat_inout': False, 'hidden_act': 'relu'}
+        },
+        {'DenseLayer': {
+            'input_dim': 2048, 'hidden_dim': 2048, 'output_dim': 4096, 'hid_drop': 0.5, 'drop_pos': 'output', 'norm': None,
+            'highway_network': None, 'cat_inout': False, 'hidden_act': 'relu'}
+        },
+        {'DenseLayer': {
+            'input_dim': 4096, 'hidden_dim': 4096, 'output_dim': 4096, 'hid_drop': 0.5, 'drop_pos': 'output', 'norm': None,
+            'highway_network': None, 'cat_inout': False, 'hidden_act': 'relu'}
+        }
+    ]
+    configs[i]['name'] = 'bL128_4pRndValid_1LinksPredValidInData_inDr.2|5xDenseLayeDr.5r(2hLay2048)_Lr5-5_200e_'+str(i)
+# -----------------------------------------------------------------------------
+for i in range(6, 7):
+    configs[i]['training']['epochs'] = 500
+    configs[i]['training']['lr'] = 5e-5
+    configs[i]['sentence_linker']['input_drop'] = 0.2
+    configs[i]['sentence_linker']['num_gen_links'] = 1
+    configs[i]['sentence_linker']['layers'] = [
+        {'DenseLayer': {
+            'input_dim': 4096, 'hidden_dim': 4096, 'output_dim': 4096, 'hid_drop': 0.5, 'drop_pos': 'output', 'norm': None,
+            'highway_network': None, 'cat_inout': False, 'hidden_act': 'relu'}
+        },
+        {'DenseLayer': {
+            'input_dim': 4096, 'hidden_dim': 4096, 'output_dim': 4096, 'hid_drop': 0.5, 'drop_pos': 'output', 'norm': None,
+            'highway_network': None, 'cat_inout': False, 'hidden_act': 'relu'}
+        },
+        {'DenseLayer': {
+            'input_dim': 4096, 'hidden_dim': 4096, 'output_dim': 4096, 'hid_drop': 0.5, 'drop_pos': 'output', 'norm': None,
+            'highway_network': None, 'cat_inout': False, 'hidden_act': 'relu'}
+        },
+        {'DenseLayer': {
+            'input_dim': 4096, 'hidden_dim': 4096, 'output_dim': 4096, 'hid_drop': 0.5, 'drop_pos': 'output', 'norm': None,
+            'highway_network': None, 'cat_inout': False, 'hidden_act': 'relu'}
+        }
+    ]
+    configs[i]['name'] = 'bL128_4pRndValid_inDr.2|4xDenseLayeDr.5r_Lr5-5_500e_'+str(i)
+# -----------------------------------------------------------------------------
+for i in range(7, 8):
+    configs[i]['training']['epochs'] = 500
+    configs[i]['training']['lr'] = 5e-5
+    configs[i]['sentence_linker']['input_drop'] = 0.2
+    configs[i]['sentence_linker']['num_gen_links'] = 1
+    configs[i]['sentence_linker']['layers'] = [
+        {'DenseLayer': {
+            'input_dim': 4096, 'hidden_dim': 4096, 'output_dim': 4096, 'hid_drop': 0.5, 'drop_pos': 'output', 'norm': None,
+            'highway_network': None, 'cat_inout': False, 'hidden_act': 'relu'}
+        },
+        {'DenseLayer': {
+            'input_dim': 4096, 'hidden_dim': 4096, 'output_dim': 4096, 'hid_drop': 0.5, 'drop_pos': 'output', 'norm': None,
+            'highway_network': None, 'cat_inout': False, 'hidden_act': 'relu'}
+        },
+        {'DenseLayer': {
+            'input_dim': 4096, 'hidden_dim': 4096, 'output_dim': 4096, 'hid_drop': 0.5, 'drop_pos': 'output', 'norm': None,
+            'highway_network': None, 'cat_inout': False, 'hidden_act': 'relu'}
+        },
+        {'DenseLayer': {
+            'input_dim': 4096, 'hidden_dim': 4096, 'output_dim': 4096, 'hid_drop': 0.5, 'drop_pos': 'output', 'norm': None,
+            'highway_network': None, 'cat_inout': False, 'hidden_act': 'relu'}
+        }
+    ]
+    configs[i]['name'] = 'bL128_4pRndValid_trSmoothL_inDr.2|4xDenseLayeDr.5r_Lr5-5_500e_'+str(i)
+
 #
 # TODO
 # [X] res_drop = [0.1, 0.3, 0.5]
--- train/train.py
+++ train/train.py
@@ -3,40 +3,25 @@ import torch.optim as optim
 # from torch.optim.lr_scheduler import lr_scheduler
 import torch.nn.functional as F
 from models.sentence_linker import SentenceLinker
-from models.sentence_linker_discriminator import SentenceLinkerDiscriminator
+# from models.sentence_linker_discriminator import SentenceLinkerDiscriminator
 from batchers.wiki_full_links_batch import WikiLinksBatch
 from torch.utils.tensorboard import SummaryWriter
 from datetime import datetime
 import time
 import sys
 from configs import configs
-from torch.autograd import Variable
+from tqdm import tqdm
 
 print(int(sys.argv[1]))
 config = configs[int(sys.argv[1])]
-
 
 if config['training']['log']:
     now = datetime.now()
     writer = SummaryWriter(log_dir="./train/logs/"+config['name'])
 
 
-# def loss_calc(output, target, reduction='mean'):
-#     loss = 0
-#     for b in range(len(target)):
-#         b_loss = 1e6
-#         for l in range(len(target[b])):
-#             bl_loss = F.mse_loss(output[b].expand(1, config['s2v_dim']),
-#                                  target[b, l].expand(1, config['s2v_dim']))
-#             if b_loss > bl_loss:
-#                 b_loss = bl_loss  # min value
-#         loss += b_loss
-#     if reduction == 'mean':
-#         loss /= len(target)
-#     return loss
-
-def loss_calc(output, target, reduction='mean'):
-    output_size = 1
+def loss_calc(output, target, reduction='mean', valid=False):
+    output_size = list(output.shape)[1]
     loss = 0
     target_cnt_list = []
     for b in target:
@@ -47,33 +32,50 @@ def loss_calc(output, target, reduction='mean'):
         target_cnt_list.append(target_cnt)
     for b in range(len(target)):
         loss_res = torch.ones((output_size, target_cnt_list[b]))
+        # l2l_loss = -1 * torch.sqrt(torch.sum(torch.pow(output[b, 0] - output[b, 1], 2))).to('cpu')
+        # l2l_loss = torch.nn.functional.threshold(l2l_loss, -1, -1)
         for o in range(output_size):
             for t in range(target_cnt_list[b]):
-                bl_loss = F.mse_loss(output[b].expand(1, config['s2v_dim']),
-                                     target[b, t].expand(1, config['s2v_dim']))
+                if valid:
+                    bl_loss = F.mse_loss(output[b, o].expand(1, config['s2v_dim']),
+                                        target[b, t].expand(1, config['s2v_dim']))
+                else:
+                    bl_loss = F.smooth_l1_loss(output[b, o].expand(1, config['s2v_dim']),
+                                        target[b, t].expand(1, config['s2v_dim']))
                 loss_res[o, t] = bl_loss
         # loss += loss_res.min()
         # print(len(target))
         # print(loss_res)
         loss_out = torch.sort(torch.min(loss_res, dim=0)[0])[0][0:output_size]
         loss += torch.mean(loss_out)
+        # if not valid:
+        #     loss += l2l_loss*0.01
     if reduction == 'mean':
         loss /= len(target)
+    # print(loss)
     return loss
 
 
 def train(model, discriminator, device, train_loader,
           optimizer, optimizer_discriminator, epoch):
     model.train()
-    discriminator_accuracy = 0
+    # discriminator_accuracy = 0
     train_loss = 0
     start = time.time()
+    pbar = tqdm(total=len(train_loader), ascii=True)
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
-        for i in range(config['training']['batch_repeat']):
+        for _ in range(config['training']['batch_repeat']):
             optimizer.zero_grad()
             # model prediction
-            output = model(data)
+            outputs = []
+            # prev_link = torch.zeros((list(data.shape)[0], config['s2v_dim']), dtype=torch.float).to(device)
+            prev_link = data
+            for _ in range(config['sentence_linker']['num_gen_links']):
+                output = model(data, prev_link)
+                outputs.append(output)
+                prev_link = output
+            output = torch.stack(outputs, dim=1)
 
             # discriminator prediction
             # discriminator_batch, discriminator_labels = dataset_train.get_discriminator_batch(data)
@@ -81,12 +83,10 @@ def train(model, discriminator, device, train_loader,
             # discriminator_labels = discriminator_labels.to(device)
             # optimizer_discriminator.zero_grad()
             # discriminator_output = discriminator(discriminator_batch)
-            # discriminator_loss = F.binary_cross_entropy_with_logits(discriminator_output,
-                                                                    # discriminator_labels)
+            # discriminator_loss = F.binary_cross_entropy_with_logits(discriminator_output, discriminator_labels)
 
             # model training
-            loss = loss_calc(output, target, reduction='mean') # - \
-    #               discriminator_loss*1e-2
+            loss = loss_calc(output, target, reduction='mean') # - discriminator_loss*1e-2
             if config['training']['l1_loss'] > 0.0:
                 l1_regularization = 0
                 for param in model.parameters():
@@ -106,6 +106,8 @@ def train(model, discriminator, device, train_loader,
             # target = discriminator_labels.argmax(dim=1)
             # corrects = (winners == target)
             # discriminator_accuracy += corrects.sum().float() / float(target.size(0))
+        pbar.update(1)
+    pbar.close()
     end = time.time()
     print("")
     # print('Epoch {}: Train set: Discriminator accuracy: {:.4f}'.format(epoch, discriminator_accuracy/(batch_idx+1)))
@@ -124,10 +126,15 @@ def test(model, device, test_loader, epoch):
     with torch.no_grad():
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
-            output = model(data)
-            test_loss += loss_calc(output, target, reduction='sum').detach()
-            # test_loss += F.mse_loss(output, target, reduction='sum').item()
-            # cosine similarity?
+            outputs = []
+            # prev_link = torch.zeros((list(data.shape)[0], config['s2v_dim']), dtype=torch.float).to(device)
+            prev_link = data
+            for _ in range(config['sentence_linker']['num_gen_links']):
+                output = model(data, prev_link)
+                outputs.append(output)
+                prev_link = output
+            output = torch.stack(outputs, dim=1) # TODO validation model generates only one link
+            test_loss += loss_calc(output, target, reduction='sum', valid=True).detach()
 
     test_loss /= len(test_loader.dataset)
     if config['training']['log']:
@@ -136,6 +143,7 @@ def test(model, device, test_loader, epoch):
 
     if epoch == 10 or True:
         print('\t\tTest set: Average loss: {:.6f}'.format(test_loss))
+    return test_loss
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -164,13 +172,21 @@ data_loader_test = torch.utils.data.DataLoader(
     shuffle=False, num_workers=4)
 
 # scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[30, 80], gamma=0.1)
+test_loss = 1e6
 for epoch in range(1, config['training']['epochs'] + 1):
     train(model, model_discriminator, device, data_loader_train,
           optimizer, optimizer_discriminator, epoch)
-    test(model, device, data_loader_test, epoch)
+    current_test_loss = test(model, device, data_loader_test, epoch)
+    if current_test_loss < test_loss:
+        test_loss = current_test_loss
+        # save model
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'loss': test_loss
+            }, './train/save/'+config['name'])
     # scheduler.step()
 
 if config['training']['log']:
     writer.close()
-
-# torch.save(model.state_dict(), "mnist_cnn.pt")
